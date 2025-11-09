dc79cf0a76c427180985f0297029ddd3d949b6f5
Olek-B <u57_alebla_waw@technischools.com>
2025-11-09 09:50:45 +0100

    releas config

---
+++ release.config.js
@@ -0,0 +1,25 @@
+const config = {
+  branches: ["main"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "docs/CHANGELOG.md",
+      },
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["dist/*.js", "dist/*.js.map", "docs/CHANGELOG.md"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+    "@semantic-release/github",
+    "semantic-release-github-actions-tags",
+  ],
+};
+
+module.exports = config;
